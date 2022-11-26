73bfb89ef789f7ae7a6afd75081ba8fdac68da1e
Stepan Seliuk <stephen.jr@selyuk.com>
2022-11-26 18:42:14 +0200

    wip Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,3 +1,27 @@
 export class Popup {
     
+    constructor(popupSel) {
+    this.selector = document.querySelector(popupSel);
+    this._closeButton = this.selector.querySelector('.popup__close');
+    this._handleEscClose = this._handleEscClose.bind(this);
+
+    this.setEventListeners();
+  }
+  setEventListeners() {
+    this._closeButton.addEventListener("mousedown", () => this.close);
+    this.selector.addEventListener("mousedown", () => this.close);
+  }
+  _handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  }
+  open() {
+    this.selector.classList.add("popup_opened");
+    window.addEventListener("keydown", this._handleEscClose);
+  }
+  close() {
+    this.selector.classList.remove("popup_opened");
+    window.removeEventListener("keydown", this._handleEscClose);
+  }
 }
