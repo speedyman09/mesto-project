451ed7812f0ee78cf7f3393ee845283802026566
Stepan Seliuk <stephen.jr@selyuk.com>
2022-11-28 14:56:38 +0200

    PopupWithForm, PopupWithImage

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,31 +1,32 @@
-import Popup from 'Popup.js'
+import Popup from "./Popup";
 export default class PopupWithForm extends Popup {
-    constructor(popupSel, callback) {
-        super(popupSel)
-        this._callback = callback
-        this._inputList = this._popup.querySelectorAll(".popup__form-input");
-        this._button = this._popup.querySelector(".popup__save-button");
-        this._formPopup = this._popup.querySelector(".popup__form");
-    }
-    _getInputValues () {
-        const inputValuesList = []
-        this._inputList.forEach((item) => {
-            inputValuesList.push(item.value)
-        })
-        return inputValuesList;
-    }
+  constructor(popupSel, callback) {
+    super(popupSel);
+    this._callback = callback;
+    this._inputList = this.PopupSel.querySelectorAll(".popup__form-input");
+    this._button = this.PopupSel.querySelector(".popup__save-button");
+    this._formPopup = this.PopupSel.querySelector(".popup__form");
+  }
 
-    setEventListeners () {
-        super.setEventListeners();
-        // обработчик сабмита форм
-        this._formPopup.addEventListener("submit", (evt) => {
-            evt.preventDefault();
-            this._callback(this._getInputValues());
-          });
-    }
+  _getInputValues() {
+    const inputValuesList = [];
+    this._inputList.forEach((item) => {
+      inputValuesList.push(item.value);
+    });
+    return inputValuesList;
+  }
 
-    close ()  {
-        super.close();
-        this._formPopup.reset();
-    }
+  setEventListeners() {
+    super.setEventListeners();
+
+    this._formPopup.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._callback(this._getInputValues());
+    });
+  }
+
+  close() {
+    super.close();
+    this._formPopup.reset();
+  }
 }
