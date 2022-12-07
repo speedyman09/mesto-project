3d67128227a009992ed88574b8f79041c552ca0d
Sergey Mishenev <mishenev-s@yandex.ru>
2022-12-07 22:53:25 +0300

    ИСПРАВЛЕНО: не изменяете текст кнопки сабмита

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,32 +1,36 @@
 export default class Popup {
-    constructor(selector) {
+  constructor(selector) {
     this.popupElem = document.querySelector(selector);
-    this.closeButton = this.popupElem.querySelector('.popup__exit');
+    this.closeButton = this.popupElem.querySelector(".popup__exit");
     this._handleEscClose = this._handleEscClose.bind(this);
+    this._btnName = this.popupElem.querySelector(".popup__save-button");
 
     this._setEventListeners();
   }
   _setEventListeners() {
-    this.closeButton.addEventListener('mousedown', () => this.close());
-    this.popupElem.addEventListener('mousedown', (evt) => {
-
+    this.closeButton.addEventListener("mousedown", () => this.close());
+    this.popupElem.addEventListener("mousedown", (evt) => {
       if (evt.target === this.popupElem) {
         this.close();
       }
-
     });
   }
+
   _handleEscClose(evt) {
-    if (evt.key === 'Escape') {
+    if (evt.key === "Escape") {
       this.close();
     }
   }
   open() {
-    this.popupElem.classList.add('popup_opened');
-    window.addEventListener('keydown', this._handleEscClose);
+    this.popupElem.classList.add("popup_opened");
+    window.addEventListener("keydown", this._handleEscClose);
+    this._initialText = this._btnName.textContent;
+    this._btnName.textContent = "Сохранение";
   }
   close() {
-    this.popupElem.classList.remove('popup_opened');
-    window.removeEventListener('keydown', this._handleEscClose);
+    this.popupElem.classList.remove("popup_opened");
+    window.removeEventListener("keydown", this._handleEscClose);
+    this._initialText = this._btnName.textContent;
+    this._btnName.textContent = "Сохранение...";
   }
 }
